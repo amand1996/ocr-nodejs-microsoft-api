d23dd098e70682b1783a04f63b986d9bcc931712
Aman Dwivedi <aman_d1996@yahoo.com>
2016-06-13 22:39:21 +0530

    Source Files Added

---
+++ OCR-NodeJS/ocr.js
@@ -0,0 +1,47 @@
+//Load the request module
+var request = require('request');
+
+var str="";
+//Lets configure and request
+request({
+    url: 'https://api.projectoxford.ai/vision/v1.0/ocr?', //URL to hit
+    qs: {"language": "unk",
+         "detectOrientation ": "true"
+         }, //Query string data
+            
+    method: 'POST', //Specify the method
+    
+    headers: { //We can define headers too
+        'Content-Type': 'application/json',
+        'Ocp-Apim-Subscription-Key':'<Subscription Key>'
+    },
+    
+    body: "{'url':'<URL TO THE IMAGE OF THE PAN CARD/DOCUMENT>'}",
+    
+}, function(error, response, body){
+    if(error) {
+        console.log(error);
+    } else {
+        var jsonObj = JSON.parse(body);
+        
+        var ob = jsonObj;
+            for(i=0;i<ob.regions.length;i++){
+                for(j=0;j<ob.regions[i].lines.length;j++){
+                    for(k=0;k<ob.regions[i].lines[j].words.length;k++){
+                            var str = str + " "+ob.regions[i].lines[j].words[k].text;
+                    }
+                    str = str + "\n";
+                }
+            }
+            
+            console.log(str);
+            
+            var arr = str.split("\n");
+            
+            console.log("Name: " + arr[1]);
+            console.log("Father's Name: " + arr[2]);
+            console.log("Date of Birth: " + arr[3]);
+            console.log("Permanent Account Number: " + arr[5]);
+            
+    }
+});
